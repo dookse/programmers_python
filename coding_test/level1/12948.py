51cdc5dd45c3cc74f24a5144eb5bf997c1a05cc3
jhpark <mugenjh@gmail.com>
2020-01-29 21:09:18 +0900

    핸드폰 번호 가리기

---
+++ coding_test/level1/12948.py
@@ -0,0 +1,10 @@
+# 핸드폰 번호 가리기
+# https://programmers.co.kr/learn/courses/30/lessons/12948?language=python3
+
+
+def solution(phone_number):
+    return '*' * (len(phone_number) - 4) + phone_number[-4:]
+
+
+print(solution('01033334444'))
+print(solution('027778888'))
