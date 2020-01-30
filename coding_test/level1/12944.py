40a14a6fa0bd600e190c0206ee22f44c1fd42289
jhpark <mugenjh@gmail.com>
2020-01-30 22:42:04 +0900

    평균 구하기

---
+++ coding_test/level1/12944.py
@@ -0,0 +1,10 @@
+# 평균 구하기
+# https://programmers.co.kr/learn/courses/30/lessons/12944?language=python3
+
+
+def solution(arr):
+    return sum(arr) / len(arr)
+
+
+print(solution([1, 2, 3, 4]))
+print(solution([5, 5]))
