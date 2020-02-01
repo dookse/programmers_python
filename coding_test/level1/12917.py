36a27eb029213c4b4554643f222148890aa05aff
jhpark <mugenjh@gmail.com>
2020-02-01 22:33:56 +0900

    문자열 내림차순으로 배치하기

---
+++ coding_test/level1/12917.py
@@ -0,0 +1,9 @@
+# 문자열 내림차순으로 배치하기
+# https://programmers.co.kr/learn/courses/30/lessons/12917?language=python3
+
+
+def solution(s):
+    return ''.join(sorted(s, reverse=True))
+
+
+print(solution('Zbcdefg'))
